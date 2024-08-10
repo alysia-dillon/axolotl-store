ba66ffa3128a7806296bf8fe97fa3fdbe1b413d2
alysiadillon <alysia.dillon@life.io>
2024-08-10 14:24:40 -0500

    adding in dummy data for multiple pet type APIs

--- src/models/types.ts
+++ src/models/types.ts
@@ -12,6 +12,54 @@ export interface AxolotlResponse {
   axolotls: Axolotl[];
 }
 
-export interface AxolotlErrorResponse {
-  error: string;
+export interface Dog {
+  id: string;
+  name: string;
+  breed: string;
+  color: string;
+  age_years: number;
+  weight_kg: number;
+}
+
+export interface DogStoreData {
+  dogs: Dog[];
+}
+
+export interface Cat {
+  id: string;
+  name: string;
+  color: string;
+  breed: string;
+  age_years: number;
+  weight_kg: number;
+}
+
+export interface CatStoreData {
+  cats: Cat[];
+}
+
+export interface FreshWaterFish {
+  id: string;
+  name: string;
+  color: string;
+  species: string;
+  age_years: number;
+  size_cm: number;
+}
+
+export interface FreshWaterFishStoreData {
+  fish: FreshWaterFish[];
+}
+
+export interface SaltWaterFish {
+  id: string;
+  name: string;
+  color: string;
+  species: string;
+  age_years: number;
+  size_cm: number;
+}
+
+export interface SaltWaterFishStoreData {
+  fish: SaltWaterFish[];
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,76 @@
 // pages/index.tsx
-import { Axolotl, AxolotlResponse } from "@deps/models/types";
-import petStoreData from "@deps/queries/dummy-data/axolotl-store.json";
+import { DEFAULT_PAGE_TITLE } from "@deps/constants/page-title";
+import { Axolotl, Cat, Dog } from "@deps/models/types";
+import axolotls from "@deps/queries/dummy-data/axolotls.json";
+import dogs from "@deps/queries/dummy-data/dogs.json";
+import cats from "@deps/queries/dummy-data/cats.json";
 
 const Home = () => {
   // Type assertion to let TypeScript know the shape of the data
-  const data = petStoreData as AxolotlResponse;
+  const availableAxolotls = axolotls;
+  const availableDogs = dogs;
+  const availableCats = cats;
 
   return (
     <div>
-      <h1>Axolotl Store</h1>
-      <ul>
-        {data.axolotls.map((axolotl: Axolotl) => (
-          <li key={axolotl.id}>
-            <strong>Name:</strong> {axolotl.name}
-            <br />
-            <strong>Color:</strong> {axolotl.color}
-            <br />
-            <strong>Type:</strong> {axolotl.type}
-            <br />
-            <strong>Size:</strong> {axolotl.size_cm} cm
-            <br />
-            <strong>Age:</strong> {axolotl.age_years} years
-            <br />
-          </li>
-        ))}
-      </ul>
+      <h1>Welcome to {DEFAULT_PAGE_TITLE}</h1>
+      <section>
+        <h2>Axolotls</h2>
+        <ul>
+          {availableAxolotls.axolotls.map((axolotl: Axolotl) => (
+            <li key={axolotl.id}>
+              <strong>Name:</strong> {axolotl.name}
+              <br />
+              <strong>Color:</strong> {axolotl.color}
+              <br />
+              <strong>Type:</strong> {axolotl.type}
+              <br />
+              <strong>Size:</strong> {axolotl.size_cm} cm
+              <br />
+              <strong>Age:</strong> {axolotl.age_years} years
+              <br />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>Dogs</h2>
+        <ul>
+          {availableDogs.dogs.map((dog: Dog) => (
+            <li key={dog.id}>
+              <strong>Name:</strong> {dog.name}
+              <br />
+              <strong>Color:</strong> {dog.breed}
+              <br />
+              <strong>Type:</strong> {dog.color}
+              <br />
+              <strong>Size:</strong> {dog.age_years} cm
+              <br />
+              <strong>Age:</strong> {dog.weight_kg} years
+              <br />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>Cats</h2>
+        <ul>
+          {availableCats.cats.map((cat: Cat) => (
+            <li key={cat.id}>
+              <strong>Name:</strong> {cat.name}
+              <br />
+              <strong>Color:</strong> {cat.breed}
+              <br />
+              <strong>Type:</strong> {cat.color}
+              <br />
+              <strong>Size:</strong> {cat.age_years} cm
+              <br />
+              <strong>Age:</strong> {cat.weight_kg} years
+              <br />
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
